897a81b19bc579626eadbbdc51975ae65fb1f03a
fengquanwei <fengquanwei1996@163.com>
2018-11-27 18:31:20 +0800

    add optional usage

---
+++ muse-guava/src/main/java/com/fengquanwei/muse/guava/basic/OptionalUsage.java
@@ -0,0 +1,17 @@
+package com.fengquanwei.muse.guava.basic;
+
+import java.util.Optional;
+
+/**
+ * Optional 用法
+ *
+ * @author fengquanwei
+ * @create 2018/11/27 18:21
+ **/
+public class OptionalUsage {
+    public static void main(String[] args) {
+        Integer a = null;
+        Integer b = Optional.ofNullable(a).orElse(0);
+        System.out.println(b);
+    }
+}
