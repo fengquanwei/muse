3e063a82b1e9b6bbd89e50793e5a6d88694bfd44
fengquanwei <fengquanwei1996@163.com>
2018-12-08 23:22:00 +0800

    add index.html

--- muse-springmvc/src/main/java/com/fengquanwei/muse/springmvc/config/SpringMvcConfig.java
+++ muse-springmvc/src/main/java/com/fengquanwei/muse/springmvc/config/SpringMvcConfig.java
@@ -1,8 +1,13 @@
 package com.fengquanwei.muse.springmvc.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
  * spring mvc 应用上下文配置
@@ -13,5 +18,23 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.fengquanwei.muse.springmvc")
-public class SpringMvcConfig {
+public class SpringMvcConfig extends WebMvcConfigurerAdapter {
+    /**
+     * 配置静态资源处理器
+     */
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    /**
+     * 内部资源视图解析器
+     */
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".html");
+        return resolver;
+    }
 }
