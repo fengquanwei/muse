05cff94417bd4058aa1e91066b8fe1eb25b606ac
fengquanwei <fengquanwei1996@163.com>
2018-12-17 16:24:57 +0800

    number util

---
+++ muse-util/src/main/java/com/fengquanwei/muse/util/NumberUtil.java
@@ -0,0 +1,32 @@
+package com.fengquanwei.muse.util;
+
+/**
+ * 数字工具类
+ *
+ * @author fengquanwei
+ * @create 2018/12/17 16:20
+ **/
+public class NumberUtil {
+    /**
+     * Integer 转二进制
+     */
+    public static String integerToBinary(Integer number) {
+        if (number == null) {
+            return null;
+        }
+
+        StringBuffer binary = new StringBuffer();
+        for (int i = 0; i < 32; i++) {
+            binary.append((number & (0x80000000 >>> i)) >>> (31 - i));
+        }
+
+        return binary.toString();
+    }
+
+    /**
+     * 测试
+     */
+    public static void main(String[] args) {
+        System.out.println(integerToBinary(123456));
+    }
+}
