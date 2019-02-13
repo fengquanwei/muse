ce6b1bfb54cbe7f68e431659b05597328d37689b
fengquanwei <fengquanwei1996@163.com>
2019-02-13 15:17:53 +0800

    curator usage

--- muse-zookeeper/src/main/java/com/fengquanwei/muse/zookeeper/curator/CuratorUsage.java
+++ muse-zookeeper/src/main/java/com/fengquanwei/muse/zookeeper/curator/CuratorUsage.java
@@ -3,6 +3,7 @@ package com.fengquanwei.muse.zookeeper.curator;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,33 +19,77 @@ public class CuratorUsage {
     /**
      * Curator 用法
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         logger.info("========== 创建会话 ==========");
 
         // 构造客户端
         CuratorFramework client = CuratorFrameworkFactory.builder()
-                .connectString("127.0.0.1:2181,127.0.0.1:2182,127.0.0.1:2183/test")
+                .connectString("127.0.0.1:2181,127.0.0.1:2182,127.0.0.1:2183")
                 .sessionTimeoutMs(5000)
                 .connectionTimeoutMs(3000)
                 .retryPolicy(new ExponentialBackoffRetry(1000, 3))
+                .namespace("curator")
                 .build();
 
         // 启动客户端
         client.start();
 
-        logger.info("connect zookeeper success");
+        logger.info("client start");
 
-        sleep(1000);
-    }
+        logger.info("========== 清理环境 ==========");
 
-    /**
-     * 休眠一会
-     */
-    private static void sleep(long millis) {
-        try {
-            Thread.sleep(millis);
-        } catch (InterruptedException e) {
-            logger.error("sleep error", e);
-        }
+        // 删除根节点及其所有子节点
+        client.delete().deletingChildrenIfNeeded().forPath("");
+
+        // 创建根节点
+        client.create().forPath("");
+
+        logger.info("clean");
+
+        logger.info("========== 创建节点 ==========");
+
+        String path1 = "/a";
+        String path2 = "/b";
+        String path3 = "/c";
+        String path4 = "/d/dd/ddd";
+        String path5 = "/e";
+
+        // 创建持久节点，初始内容为空
+        client.create().forPath(path1);
+        logger.info("create node, path: {}", path1);
+
+        // 创建持久节点，附带初始内容
+        client.create().forPath(path2, "B".getBytes());
+        logger.info("create node, path: {}", path2);
+
+        // 创建临时节点
+        client.create().withMode(CreateMode.EPHEMERAL).forPath(path3, "C".getBytes());
+        logger.info("create node, path: {}", path3);
+
+        // 递归创建父节点
+        client.create().creatingParentsIfNeeded().forPath(path4, "D".getBytes());
+        logger.info("create node, path: {}", path4);
+
+        client.create().forPath(path5);
+        logger.info("create node, path: {}", path5);
+
+        logger.info("========== 删除节点 ==========");
+
+        // 删除叶子节点
+        client.delete().forPath(path1);
+        logger.info("delete node, path: {}", path1);
+
+        // 指定版本删除节点
+        client.delete().withVersion(-1).forPath(path2);
+        logger.info("delete node, path: {}", path2);
+
+        // 删除节点并递归删除其所有叶子节点
+        String parentPath = path4.substring(0, path4.indexOf("/", 1));
+        client.delete().deletingChildrenIfNeeded().forPath(parentPath);
+        logger.info("delete node, path: {}", parentPath);
+
+        // 强制删除节点
+        client.delete().guaranteed().forPath(path5);
+        logger.info("delete node, path: {}", path5);
     }
 }
