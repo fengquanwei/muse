8e27b477dff50add5c466b5fe376e586f567731a
fengquanwei <fengquanwei1996@163.com>
2019-02-21 11:13:32 +0800

    curator recipes usage

--- muse-zookeeper/src/main/java/com/fengquanwei/muse/zookeeper/curator/RecipesUsage.java
+++ muse-zookeeper/src/main/java/com/fengquanwei/muse/zookeeper/curator/RecipesUsage.java
@@ -2,16 +2,27 @@ package com.fengquanwei.muse.zookeeper.curator;
 
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.atomic.AtomicValue;
+import org.apache.curator.framework.recipes.atomic.DistributedAtomicInteger;
+import org.apache.curator.framework.recipes.barriers.DistributedDoubleBarrier;
 import org.apache.curator.framework.recipes.cache.ChildData;
 import org.apache.curator.framework.recipes.cache.NodeCache;
 import org.apache.curator.framework.recipes.cache.PathChildrenCache;
 import org.apache.curator.framework.recipes.cache.PathChildrenCacheEvent;
 import org.apache.curator.framework.recipes.leader.LeaderSelector;
 import org.apache.curator.framework.recipes.leader.LeaderSelectorListenerAdapter;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
 import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.curator.retry.RetryNTimes;
 import org.apache.zookeeper.CreateMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * 典型使用场景
@@ -20,7 +31,7 @@ import org.slf4j.LoggerFactory;
  * @create 2019/2/13 18:21
  **/
 public class RecipesUsage {
-    private static Logger logger = LoggerFactory.getLogger(RecipesUsage.class);
+    private static final Logger logger = LoggerFactory.getLogger(RecipesUsage.class);
 
     /**
      * 典型使用场景
@@ -144,10 +155,111 @@ public class RecipesUsage {
         leaderSelector3.autoRequeue();
         leaderSelector3.start();
 
-        Thread.sleep(10000);
+        Thread.sleep(6000);
         leaderSelector1.close();
         leaderSelector2.close();
         leaderSelector3.close();
 
+        logger.info("========== 分布式锁 ==========");
+
+        String lockPath = "/lock";
+        final CountDownLatch lockCountDownLatch = new CountDownLatch(1);
+
+        // 分布式锁
+        final InterProcessMutex lock = new InterProcessMutex(client, lockPath);
+
+        for (int i = 0; i < 30; i++) {
+            new Thread(() -> {
+                try {
+                    // 预备
+                    lockCountDownLatch.await();
+                    // 获取锁
+                    lock.acquire();
+                } catch (Exception e) {
+                    logger.error("acquire lock error", e);
+                }
+
+                String orderNo = new SimpleDateFormat("HH:mm:ss.SSS").format(new Date());
+                logger.info("order no: {}", orderNo);
+
+                // 释放锁
+                try {
+                    lock.release();
+                } catch (Exception e) {
+                    logger.error("release lock error", e);
+                }
+            }).start();
+        }
+
+        // 开始
+        lockCountDownLatch.countDown();
+
+        Thread.sleep(1000);
+
+        logger.info("========== 分布式计数器 ==========");
+
+        String counterPath = "/counter";
+        final CountDownLatch counterCountDownLatch = new CountDownLatch(10);
+
+        // 分布式计数器
+        final DistributedAtomicInteger distributedAtomicInteger = new DistributedAtomicInteger(client, counterPath, new RetryNTimes(3, 1000));
+
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                AtomicValue<Integer> atomicValue = null;
+
+                while (atomicValue == null || !atomicValue.succeeded()) {
+                    try {
+                        atomicValue = distributedAtomicInteger.add(1);
+                        logger.info("add succeeded: {}, pre value: {}, post value: {}", atomicValue.succeeded(), atomicValue.preValue(), atomicValue.postValue());
+                    } catch (Exception e) {
+                        logger.error("add error", e);
+                    }
+                }
+
+                counterCountDownLatch.countDown();
+            }).start();
+        }
+
+        counterCountDownLatch.await();
+
+        logger.info("========== 分布式 Barrier ==========");
+
+        final String barrierPath = "/barrier";
+
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        executorService.submit(new Worker("1 号选手", barrierPath, client));
+        executorService.submit(new Worker("2 号选手", barrierPath, client));
+        executorService.submit(new Worker("3 号选手", barrierPath, client));
+        executorService.shutdown();
+    }
+
+    /**
+     * 分布式 barrier 工人
+     */
+    static class Worker implements Runnable {
+        private String name;
+        private String path;
+        private CuratorFramework client;
+
+        public Worker(String name, String path, CuratorFramework client) {
+            this.name = name;
+            this.path = path;
+            this.client = client;
+        }
+
+        @Override
+        public void run() {
+            try {
+                DistributedDoubleBarrier barrier = new DistributedDoubleBarrier(client, path, 3);
+                logger.info(this.name + " ready");
+                barrier.enter();
+                logger.info(this.name + " enter");
+                barrier.leave();
+                logger.info(this.name + " leave");
+            } catch (Exception e) {
+                logger.error("await error", e);
+            }
+        }
     }
 }
