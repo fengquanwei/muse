707ea573fc38212e5e108d25ffa226b0bd08a0fc
fengquanwei <fengquanwei1996@163.com>
2019-03-31 17:14:43 +0800

    update

--- muse-zookeeper/src/main/java/com/fengquanwei/muse/zookeeper/curator/CuratorClientUsage.java
+++ muse-zookeeper/src/main/java/com/fengquanwei/muse/zookeeper/curator/CuratorClientUsage.java
@@ -41,7 +41,7 @@ public class CuratorClientUsage {
         // 启动客户端
         client.start();
 
-        logger.info("client start");
+        logger.info("创建会话成功");
 
         logger.info("========== 清理环境 ==========");
 
@@ -51,7 +51,7 @@ public class CuratorClientUsage {
         // 创建根节点
         client.create().forPath("");
 
-        logger.info("clean");
+        logger.info("清理环境完成");
 
         logger.info("========== 创建节点 ==========");
 
@@ -63,62 +63,63 @@ public class CuratorClientUsage {
 
         // 创建持久节点，初始内容为空
         client.create().forPath(path1);
-        logger.info("create node, path: {}", path1);
+        logger.info("创建节点, 节点路径: {}", path1);
 
         // 创建持久节点，附带初始内容
         client.create().forPath(path2, "B".getBytes());
-        logger.info("create node, path: {}", path2);
+        logger.info("创建节点, 节点路径: {}", path2);
 
         // 创建临时节点
         client.create().withMode(CreateMode.EPHEMERAL).forPath(path3, "C".getBytes());
-        logger.info("create node, path: {}", path3);
+        logger.info("创建节点, 节点路径: {}", path3);
 
         // 递归创建父节点
         client.create().creatingParentsIfNeeded().forPath(path4, "D".getBytes());
-        logger.info("create node, path: {}", path4);
+        logger.info("创建节点, 节点路径: {}", path4);
 
+        // 创建持久节点，初始内容为空
         client.create().forPath(path5);
-        logger.info("create node, path: {}", path5);
+        logger.info("创建节点, 节点路径: {}", path5);
 
         logger.info("========== 读取数据 ==========");
 
         // 读取节点数据
         byte[] data2 = client.getData().forPath(path2);
-        logger.info("get data, path: {}, data: {}", path2, new String(data2));
+        logger.info("读取数据, 节点路径: {}, 节点数据: {}", path2, new String(data2));
 
         // 读取节点数据同时获取 stat
         Stat stat3 = new Stat();
         byte[] data3 = client.getData().storingStatIn(stat3).forPath(path3);
-        logger.info("get data, path: {}, data: {}, stat: {}", path3, new String(data3), stat3);
+        logger.info("读取数据, 节点路径: {}, 节点数据: {}, 节点状态: {}", path3, new String(data3), stat3);
 
         logger.info("========== 更新数据 ==========");
 
         // 更新数据
         Stat stat22 = client.setData().forPath(path2, "BB".getBytes());
-        logger.info("set data, path: {}, data: {}, stat: {}", path2, new String(client.getData().forPath(path2)), stat22);
+        logger.info("更新数据, 节点路径: {}, 节点数据: {}, 节点状态: {}", path2, new String(client.getData().forPath(path2)), stat22);
 
         // 指定版本更新
         Stat stat222 = client.setData().withVersion(stat22.getVersion()).forPath(path2, "BBB".getBytes());
-        logger.info("set data, path: {}, data: {}, stat: {}", path2, new String(client.getData().forPath(path2)), stat222);
+        logger.info("更新数据, 节点路径: {}, 节点数据: {}, 节点状态: {}", path2, new String(client.getData().forPath(path2)), stat222);
 
         logger.info("========== 删除节点 ==========");
 
         // 删除叶子节点
         client.delete().forPath(path1);
-        logger.info("delete node, path: {}", path1);
+        logger.info("删除节点, 节点路径: {}", path1);
 
         // 指定版本删除节点
         client.delete().withVersion(-1).forPath(path2);
-        logger.info("delete node, path: {}", path2);
+        logger.info("删除节点, 节点路径: {}", path2);
 
         // 删除节点并递归删除其所有叶子节点
         String parentPath = path4.substring(0, path4.indexOf("/", 1));
         client.delete().deletingChildrenIfNeeded().forPath(parentPath);
-        logger.info("delete node, path: {}", parentPath);
+        logger.info("递归删除节点, 父节点路径: {}", parentPath);
 
         // 强制删除节点
         client.delete().guaranteed().forPath(path5);
-        logger.info("delete node, path: {}", path5);
+        logger.info("强制删除节点, 节点路径: {}", path5);
 
         logger.info("========== 异步接口 ==========");
 
@@ -128,7 +129,7 @@ public class CuratorClientUsage {
         client.create().withMode(CreateMode.EPHEMERAL).inBackground(new BackgroundCallback() {
             @Override
             public void processResult(CuratorFramework curatorFramework, CuratorEvent curatorEvent) throws Exception {
-                logger.info("【{}】process result, curatorEvent: {}", Thread.currentThread().getName(), curatorEvent);
+                logger.info("【BackgroundCallback1】处理异步结果, 当前线程: {}, 事件: {}", Thread.currentThread().getName(), curatorEvent);
                 countDownLatch.countDown();
             }
         }).forPath("/f", "F".getBytes());
@@ -138,12 +139,14 @@ public class CuratorClientUsage {
         client.create().withMode(CreateMode.EPHEMERAL).inBackground(new BackgroundCallback() {
             @Override
             public void processResult(CuratorFramework curatorFramework, CuratorEvent curatorEvent) throws Exception {
-                logger.info("【{}】process result, curatorEvent: {}", Thread.currentThread().getName(), curatorEvent);
+                logger.info("【BackgroundCallback2】处理异步结果, 当前线程: {}, 事件: {}", Thread.currentThread().getName(), curatorEvent);
                 countDownLatch.countDown();
             }
         }, executorService).forPath("/f", "F".getBytes());
 
         countDownLatch.await();
         executorService.shutdown();
+
+        Thread.sleep(100000000);
     }
 }
