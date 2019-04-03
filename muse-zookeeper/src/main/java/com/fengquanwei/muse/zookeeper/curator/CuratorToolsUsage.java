dd204488691fda4df7554b12f7f31b1c52d655a7
fengquanwei <fengquanwei1996@163.com>
2019-04-03 20:33:19 +0800

    update

--- muse-zookeeper/src/main/java/com/fengquanwei/muse/zookeeper/curator/CuratorToolsUsage.java
+++ muse-zookeeper/src/main/java/com/fengquanwei/muse/zookeeper/curator/CuratorToolsUsage.java
@@ -37,37 +37,47 @@ public class CuratorToolsUsage {
         // 启动客户端
         client.start();
 
-        logger.info("client start");
+        logger.info("创建会话成功");
 
         logger.info("========== ZKPaths ==========");
 
         String namespace = "/tools";
-        String p1 = ZKPaths.fixForNamespace(namespace, "sub");
-        String p2 = ZKPaths.makePath(namespace, "sub");
-        String n1 = ZKPaths.getNodeFromPath(namespace + "/aa/aaa");
+
+        String path1 = ZKPaths.fixForNamespace(namespace, "sub");
+        logger.info("fixForNamespace: {}", path1);
+
+        String path2 = ZKPaths.makePath(namespace, "sub");
+        logger.info("makePath: {}", path2);
+
+        String node1 = ZKPaths.getNodeFromPath(namespace + "/aa/aaa");
+        logger.info("getNodeFromPath: {}", node1);
 
         ZKPaths.PathAndNode pathAndNode = ZKPaths.getPathAndNode(namespace + "/aa/aaa");
-        String p = pathAndNode.getPath();
-        String n = pathAndNode.getNode();
+        String path3 = pathAndNode.getPath();
+        String node3 = pathAndNode.getNode();
+        logger.info("pathAndNode, path: {}, node: {}", path3, node3);
 
         ZooKeeper zooKeeper = client.getZookeeperClient().getZooKeeper();
 
-        String path1 = namespace + "/child1";
-        String path2 = namespace + "/child2";
-        ZKPaths.mkdirs(zooKeeper, path1);
-        ZKPaths.mkdirs(zooKeeper, path2);
+        String path4 = namespace + "/child1";
+        String path5 = namespace + "/child2";
+        ZKPaths.mkdirs(zooKeeper, path4);
+        ZKPaths.mkdirs(zooKeeper, path5);
         List<String> sortedChildren = ZKPaths.getSortedChildren(zooKeeper, namespace);
+        logger.info("sortedChildren: {}", sortedChildren);
 
-        ZKPaths.deleteChildren(zooKeeper, namespace, true);
+        ZKPaths.deleteChildren(zooKeeper, namespace, false);
 
         logger.info("========== EnsurePaths ==========");
 
-        client.usingNamespace("tools");
-
-        EnsurePath ensurePath1 = new EnsurePath("/p");
+        String path6 = ZKPaths.fixForNamespace(namespace, "/p");
+        EnsurePath ensurePath1 = new EnsurePath(path6);
         ensurePath1.ensure(client.getZookeeperClient());
+        logger.info("静默创建节点, 节点路径: {}", path6);
 
-        EnsurePath ensurePath2 = client.newNamespaceAwareEnsurePath("/q");
+        String path7 = ZKPaths.fixForNamespace(namespace, "/q");
+        EnsurePath ensurePath2 = client.newNamespaceAwareEnsurePath(path7);
         ensurePath2.ensure(client.getZookeeperClient());
+        logger.info("静默创建节点, 节点路径: {}", path7);
     }
 }
