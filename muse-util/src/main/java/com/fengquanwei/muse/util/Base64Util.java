8daa741741900b299678838c862e1d3fe433b3b9
fengquanwei <fengquanwei1996@163.com>
2019-04-23 19:40:15 +0800

    update

--- muse-util/src/main/java/com/fengquanwei/muse/util/Base64Util.java
+++ muse-util/src/main/java/com/fengquanwei/muse/util/Base64Util.java
@@ -18,6 +18,19 @@ public class Base64Util {
     private static Logger logger = LoggerFactory.getLogger(Base64Util.class);
 
     /**
+     * URL 安全的 BASE64 编码
+     */
+    public static String urlSafeEncode(byte[] data) {
+        String encode = encode(data);
+
+        if (encode == null || encode.length() == 0) {
+            return null;
+        }
+
+        return encode.replaceAll("\\+", "-").replaceAll("/", "_").replaceAll("=", "\\.");
+    }
+
+    /**
      * BASE64 编码
      */
     public static String encode(byte[] data) {
@@ -47,17 +60,31 @@ public class Base64Util {
     }
 
     /**
+     * URL 安全的 BASE64 解码
+     */
+    public static byte[] urlSafeDecode(String data) {
+        if (data == null || data.length() == 0) {
+            return null;
+        }
+
+        data = data.replaceAll("-", "\\+").replaceAll("_", "/").replaceAll("\\.", "=");
+
+        return decode(data);
+    }
+
+    /**
      * 测试
      */
     public static void main(String[] args) {
-        byte[] data = "hello world".getBytes(StandardCharsets.UTF_8);
+        byte[] data = ("URL 安全的编码！").getBytes(StandardCharsets.UTF_8);
 
         // 编码
-        String encodeString = Base64Util.encode(data);
+        String encodeString = Base64Util.urlSafeEncode(data);
+        System.out.println(encode(data));
         System.out.println(encodeString);
 
         // 解码
-        byte[] decodeBytes = Base64Util.decode(encodeString);
+        byte[] decodeBytes = Base64Util.urlSafeDecode(encodeString);
         System.out.println(new String(decodeBytes, StandardCharsets.UTF_8));
     }
 }
